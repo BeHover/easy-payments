03ef8e47a4a1bf3eed23051a967e1a26440bc7ce
@ihor.berest <ihor.berest@paybis.com>
2022-04-16 19:25:05 +0300

    refactor login controller

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,70 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Repository\ApartmentRepository;
-use App\Repository\DistrictRepository;
-use App\Repository\HouseRepository;
-use App\Repository\UserRepository;
+use App\Exceptions\UserValidationException;
+use App\Service\UserLoginService;
+use Doctrine\ORM\EntityNotFoundException;
 use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 
 class LoginController extends AbstractController
 {
-    private UserRepository $userRepository;
-    private ApartmentRepository $apartmentRepository;
-    private HouseRepository $houseRepository;
-    private DistrictRepository $districtRepository;
-    private UserPasswordHasherInterface $passwordHasher;
+    private UserLoginService $loginService;
     private string $jwtKey;
 
     public function __construct(
-        UserRepository $userRepository,
-        ApartmentRepository $apartmentRepository,
-        HouseRepository $houseRepository,
-        DistrictRepository $districtRepository,
-        UserPasswordHasherInterface $passwordHasher,
+        UserLoginService $loginService,
         string  $jwtKey
     ) {
-        $this->userRepository = $userRepository;
-        $this->apartmentRepository = $apartmentRepository;
-        $this->houseRepository = $houseRepository;
-        $this->districtRepository = $districtRepository;
-        $this->passwordHasher = $passwordHasher;
+        $this->loginService = $loginService;
         $this->jwtKey = $jwtKey;
     }
 
     /**
-     * @Route(path="/login", name="login")
+     * @Route(path="/login", name="login", methods={"POST"})
+     * @throws UserValidationException|EntityNotFoundException|UserNotFoundException
      */
     public function login(Request $request): JsonResponse
     {
-        $district = $request->get('district');
-        $street = $request->get('street');
-        $houseNumber = $request->get('house-number');
-        $apartmentNumber = $request->get('apartment-number');
-        $code = $request->get('code');
-
-        $district = $this->districtRepository->findOneBy(['name' => $district]);
-        $house = $this->houseRepository->findOneBy(['district' => $district, 'address' => $street, 'number' => $houseNumber]);
-        $apartment = $this->apartmentRepository->findOneBy(['house' => $house, 'number' => $apartmentNumber]);
-
-        $user = $this->userRepository->findOneBy(['apartment' => $apartment]);
-
-        if (!$this->passwordHasher->isPasswordValid($user, $code)) {
-            throw new BadCredentialsException(sprintf(
-                'Wrong code for given data: district %s, street %s, house number %d, apartment number $d, code %s',
-                $district->getName(),
-                $house->getAddress(),
-                $house->getNumber(),
-                $code
-            ));
-        }
+        $user = $this->loginService->getUserFromLoginRequest($request);
+        $this->loginService->validateUserPassword($user, $request->get('password'));
 
         $token = JWT::encode(['uuid' => $user->getUuid()], $this->jwtKey, 'HS256');
 
---
+++ src/Service/UserLoginService.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Exceptions\UserValidationException;
+use App\Repository\ApartmentRepository;
+use App\Repository\DistrictRepository;
+use App\Repository\HouseRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityNotFoundException;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Exception\BadCredentialsException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+
+class UserLoginService
+{
+    private UserRepository $userRepository;
+    private ApartmentRepository $apartmentRepository;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(
+        UserRepository $userRepository,
+        ApartmentRepository $apartmentRepository,
+        UserPasswordHasherInterface $passwordHasher
+    ) {
+       $this->userRepository = $userRepository;
+       $this->apartmentRepository = $apartmentRepository;
+       $this->passwordHasher = $passwordHasher;
+    }
+
+
+    /**
+     * @throws EntityNotFoundException|UserNotFoundException
+     */
+    public function getUserFromLoginRequest(Request $loginRequest): User
+    {
+        if (!$loginRequest->request->has('apartmentId')) {
+            throw new BadRequestException('Request don\'t provide apartmentId post argument!');
+        }
+
+        $apartmentId = $loginRequest->request->get('apartmentId');
+        $apartment = $this->apartmentRepository->find($apartmentId);
+
+        if (null === $apartment) {
+            throw new EntityNotFoundException(sprintf(
+                'Apartment not found: id %d',
+                $apartmentId
+            ));
+        }
+
+        $user = $this->userRepository->findOneBy(['apartment' => $apartment]);
+
+        if (null === $user) {
+            throw new UserNotFoundException(sprintf(
+                'User not found by apartmentId %d',
+                $apartmentId
+            ));
+        }
+
+        return $user;
+    }
+
+    /**
+     * @throws UserValidationException
+     */
+    public function validateUserPassword(User $user, string $password): void
+    {
+        if (!$this->passwordHasher->isPasswordValid($user, $password)) {
+            throw new UserValidationException(sprintf(
+                'User validation error: user with uuid: $s was given wrong password %s',
+                $user->getUuid(),
+                $password
+            ));
+        }
+    }
+}
